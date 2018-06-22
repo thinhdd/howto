27ce57cf0e2601095511820301cf222c3d7c0b39
Casper <mr.thinhdaoduc@gmail.com>
2018-06-23 03:16:02 +0700

    Multi module and share module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {ChildComponent} from './child/child.component';
+import {DataService} from './data.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <p>==================================</p>
+    <p>Message: {{ message }}</p>
+    <p>Message send by output: {{childOutput}}</p>
+    <button (click)="newMessage()">New Message to other subscriber</button>
+    <p>==================================</p>
+    <app-child [childMessage]="parentMessage" (messageEvent)="receiveMessage($event)"></app-child>
+  `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewInit {
+  message: String = '';
+  childOutput: String = '';
+  @ViewChild(ChildComponent) childComponent: ChildComponent;
+  parentMessage = 'message from parent';
+  constructor(private data: DataService) {  }
+
+  ngAfterViewInit() {
+    this.message = this.childComponent.message;
+  }
+  receiveMessage($event) {
+    this.childOutput = $event;
+  }
+
+  newMessage() {
+    this.data.changeMessage('Hello from Sibling');
+  }
 }
