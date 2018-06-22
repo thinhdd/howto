797803f6c01b1b8d0d6bec6deb444007a75b5a39
Casper <mr.thinhdaoduc@gmail.com>
2018-06-23 02:37:29 +0700

    Multi module and share module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AdService } from './ad.service';
+import { AdItem } from './ad-item';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-my',
+  template: `
+    <div>
+      <app-add-banner [ads]="ads"></app-add-banner>
+    </div>
+  `
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  ads: AdItem[];
+
+  constructor(private adService: AdService) {}
+
+  ngOnInit() {
+    this.ads = this.adService.getAds();
+  }
 }
+
