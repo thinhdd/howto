0512f9e222567cc4bfe8b8a8e4bdeb89891a774f
Casper <galzog@gmail.com>
2018-06-22 17:44:19 +0700

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HEROES} from './constant/heroes';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  heroes = HEROES;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {BiddingExComponent} from './bidding-ex/bidding-ex.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BiddingExComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
